bf7768513fcc64bbd547418adb51f6370838b064
PolinaHolotsvan <swan.polina00@gmail.com>
2022-11-28 17:12:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿interface IItem
+{
+    void SetPrice(double price);
+}
+
+interface IDiscount
+{
+    void ApplyDiscount(String discount);
+}
+
+interface IPromocode
+{
+    void ApplyPromocode(String promocode);
+}
+
+interface IClothes
+{
+    void SetColor(byte color);
+    void SetSize(byte size);
+}
+class Book : IItem, IDiscount
+{
+    public void SetPrice(double price)
+    {
+        ///
+    }
+    public void ApplyDiscount(String discount)
+    {
+        ///
+    }
+
+}
+
+class Clothes : IItem, IClothes, IDiscount
+{
+    public void SetPrice(double price)
+    {
+        ///
+    }
+
+    public void ApplyDiscount(String discount)
+    {
+        ///
+    }
+
+    public void SetColor(byte color)
+    {
+        ///
+    }
+    public void SetSize(byte size)
+    {
+        ///
+    }
+}
